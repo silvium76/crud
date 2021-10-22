4c50bacbfdc7e38199cca11d49b6bf52fe625abe
Silviu Marincas <marincas.silviu@gmail.com>
2021-10-22 11:45:21 +0300

    Migrate from SQLite to PostgreSQL

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -76,9 +76,16 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        'NAME': 'posts',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost'
     }
 }
 
